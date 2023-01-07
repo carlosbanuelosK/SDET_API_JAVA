262ced71fc66901da49156eb58c5d3361f261678
Carlos <carlos.banuelos@unosquare.com>
2023-01-07 14:42:56 -0600

    final

--- src/test/java/com/unosquare/Encapsulation.java
+++ src/test/java/com/unosquare/Encapsulation.java
@@ -2,22 +2,37 @@ package com.unosquare;
 
 import java.io.IOException;
 
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 import resources.TestDataBuild;
 
 public class Encapsulation {
-	RequestSpecification res;
-	ResponseSpecification resspec;
-	Response response;
-	TestDataBuild data = new TestDataBuild();
+
+	
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//POST
 		String test = TestDataBuild.Add("C:\\Users\\carlos.banuelos\\eclipse-workspace\\API\\Json\\Create.json");
+		String test2 = TestDataBuild.Add("C:\\Users\\carlos.banuelos\\eclipse-workspace\\API\\Json\\Register.json");
+		String test3 = TestDataBuild.Add("C:\\Users\\carlos.banuelos\\eclipse-workspace\\API\\Json\\Register.json");
+		String test4 = TestDataBuild.Add("C:\\Users\\carlos.banuelos\\eclipse-workspace\\API\\Json\\Register.json");
+
 		System.out.println(test);
-
+		System.out.println(test2);
+		System.out.println(test3);
+		System.out.println(test4);
+		
+		System.out.println("-------------------------------- Get API ---------------------------------");
+		//GET
+		TestDataBuild.Get1();
+		TestDataBuild.Get2();
+		TestDataBuild.Get3();
+		TestDataBuild.Get4();
+		
+		System.out.println("-------------------------------- Update API ---------------------------------");
+		
+		String update = TestDataBuild.Add("C:\\Users\\carlos.banuelos\\eclipse-workspace\\API\\Json\\Update.json");
+		System.out.println(update);
+		
+		
 	}
-
 }
--- src/test/java/resources/TestDataBuild.java
+++ src/test/java/resources/TestDataBuild.java
@@ -17,9 +17,64 @@ public class TestDataBuild {
 		String response = given().log().all().body(new String(Files.readAllBytes(Paths.get(path)))).when()
 				.post("/api/users").then().log().all().assertThat().statusCode(201).extract().response().asString();
 		return response;
-
-		
-		
+	}
+	
+	public static String Add2(String path) throws IOException {
+		RestAssured.baseURI ="https://reqres.in/";
+		String response = given().log().all()
+		.body(new String(Files.readAllBytes(Paths.get(path))))
+		.when().post("/api/register")
+		.then().log().all().assertThat().statusCode(400).extract().response().asString();
+		return response;
+	}
+	
+	public static String Add3(String path) throws IOException {
+		RestAssured.baseURI ="https://reqres.in/";
+		String response = given().log().all()
+		.body(new String(Files.readAllBytes(Paths.get(path))))
+		.when().post("/api/register")
+		.then().log().all().assertThat().statusCode(400).extract().response().asString();
+		return response;
+	}
+	
+	public static String Add4(String path) throws IOException {
+		RestAssured.baseURI ="https://reqres.in/";
+		String response = given().log().all()
+		.body(new String(Files.readAllBytes(Paths.get(path))))
+		.when().post("/api/login")
+		.then().log().all().assertThat().statusCode(400).extract().response().asString();
+		return response;
+	}
+	
+	public static void Get1() {
+		given().log().all()
+		 .when().get("https://reqres.in/api/users?page=2")
+		 .then().assertThat().log().all().statusCode(200).extract().asString();
+	}
+	
+	public static void Get2() {
+		given().log().all()
+		 .when().get("https://reqres.in/api/users/2")
+		 .then().assertThat().log().all().statusCode(200).extract().asString();
+	}
+	public static void Get3() {
+		given().log().all()
+		 .when().get("https://reqres.in/api/users/23")
+		 .then().assertThat().log().all().statusCode(404).extract().asString();
+	}
+	public static void Get4() {
+		given().log().all()
+		 .when().get("https://reqres.in/api/unknown")
+		 .then().assertThat().log().all().statusCode(200).extract().asString();
+	}
+	
+	public static String Update(String path) throws IOException {
+		RestAssured.baseURI ="https://reqres.in/";
+		String response = given().log().all()
+		.body(new String(Files.readAllBytes(Paths.get(path))))
+		.when().put("/api/user/2")
+		.then().log().all().assertThat().statusCode(400).extract().response().asString();
+		return response;
 	}
 
 }
